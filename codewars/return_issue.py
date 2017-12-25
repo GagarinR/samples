ccc52b9193f1ddcb192a1b12ce3d33cfa64da5f3
TheRutubeify <elitegroup.kz@gmail.com>
2017-12-25 12:49:22 -0800

    Update return_issue.py

--- codewars/return_issue.py
+++ codewars/return_issue.py
@@ -1,3 +1,11 @@
+"""
+Task
+Given two cells on the standard chess board, determine whether they have the same color or not.
+
+Example
+For cell1 = "A1" and cell2 = "C3", the output should be true.
+"""
+
 def chess_board_cell_color(cell1, cell2):
     c = lambda x: x.replace('A','1').replace('B','2').replace('C','3').replace('D','4').replace('E','5').replace('F','6').replace('G','7').replace('H','8')
 
