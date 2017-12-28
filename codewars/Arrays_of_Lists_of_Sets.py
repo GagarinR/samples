772c1d596a6ea31285444e2cbe015236bf40dcf0
TheRutubeify <elitegroup.kz@gmail.com>
2017-12-27 16:37:55 -0800

    Create Arrays_of_Lists_of_Sets.py

---
+++ codewars/Arrays_of_Lists_of_Sets.py
@@ -0,0 +1,32 @@
+    
+s1 = ["abc","abbc","ab","xyz","xy","zzyx"]#),[1,8])
+s2 = ['wkskkkkkk','fokoo','wkskk','uizzzz','fokooff','wkskkkk','uizzzzzzz']#),[5,7,9])
+
+def solve(arr):    
+    arr=[''.join(set([i2 for i2 in i])) for i in arr]
+    
+    r=[]
+    for c, i in enumerate(arr):
+        for c2, i2 in enumerate(arr):
+            if i == i2:
+                 r.append({i2: c2})
+
+    new_arr =  [i.keys()[0] for i in r]
+    new_arr = [i for i in new_arr if new_arr.count(i)==1 ]
+    
+    r2 =[]
+    for i in r:
+        if i not in r2:
+            r2.append(i)
+
+    t = dict()
+    for i in r2:
+        if i.keys()[0] not in t.keys(): t[i.keys()[0]] = i[i.keys()[0]]
+        else: t[i.keys()[0]]+=i[i.keys()[0]]
+
+    for i in new_arr: 
+        del t[i]
+    
+    return sorted([t[i] for i in t])
+    
+print solve(s1)
